2729370071f887ab7bcf630ab7482aea3aa1c128
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2022-12-28 15:54:15 +0500

    update

---
+++ tasks/javascript.js
@@ -0,0 +1,50 @@
+
+// Begin 1
+// let a = 5;
+// console.log( `P = ${4 * a}`);
+
+
+// Begin 2
+// let b = 5;
+// console.log(`S = ${b ** 2}`);
+
+// Begin 3
+// let a = 5;
+// let b = 6
+// console.log(`S = ${a*b}`);
+// console.log(`P = ${2 * (a+b)}`);
+
+
+// Begin4
+// let p = 3.14
+// let d = 5;
+// console.log(`L = ${p*d}`);
+
+// Begin 5
+// let a = 2
+// console.log(`V = ${a ** 3}`)
+// console.log(`S = ${6 * (a ** 2)}`);
+
+// Begin 6
+// let a = 2
+// let b = 3
+// let c = 4
+// console.log(`V = ${a*b*c}`);
+// console.log(`S = ${2*(a*b+b*c+a*c)}`);
+
+// Begin 7
+// let r = 3
+// let p = 3.14
+// console.log(`L = ${2*p*r}`);
+// console.log(`S = ${p*(r**2)}`);
+
+// Begin 8
+// let a = 2
+// let b = 3
+// let c = 5
+// console.log(`${Math.sqrt(2)}`);
+
+
+
+
+
