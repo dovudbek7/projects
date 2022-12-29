d461f5b23e78396659b0d320728d62750cf00fc4
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2022-12-29 20:28:18 +0500

    update

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -45,6 +45,89 @@
 // console.log(`${Math.sqrt(2)}`);
 
 
+// Begin 9
+// let a = 5
+// let b = 5
+// let c = a + b
+// console.log(`${Math.sqrt(c)}`);
+
+// Begin 10
+// let a = 5
+// let b = 5
+// console.log(a+b);
+// console.log(a*b);
+// console.log((a**2),(b**2));
+
+// Begin 11
+// let a = 5
+// let b = 5
+// console.log(a+b);
+// console.log(a*b);
+
+// Begin 12
+// let a = 2
+// let b = 2
+// let d = ((a**2)+(b**2))
+// let c = Math.sqrt(d)
+// let p = (a+b+c);
+// console.log(c,p);
+
+// Begin 13
+// let r1 = 3
+// let r2 = 4
+// let p = 3.4
+// let s1 = p*r1
+// let s2 = p*r2
+// let s3 = p*(r1-r2)
+// console.log(s1,s2,s3);
+
+// Begin 14
+// let l = 2
+// let p = 3.14
+// let r = 2 * p * l
+// let s = p * (r**2)
+// console.log(r,s);
+
+// Begin 15
+// let l = 2
+// let p = 3.14
+// let r = 2 * p * l
+// let s = p * (r**2)
+// console.log(r,s);
+
+// Begin 16
+// let x1 = 5
+// let x2 = 3
+// let x = x1-x2
+// console.log(x);
+
+// Begin 17
+// let a = 5
+// let b = 2
+// let c = 3
+// let i = a+c
+// let d = a+b
+// let f = a+b+c
+// console.log(i,d,f);
+
+// Begin 18
+// let a = 5
+// let c = 3
+// let b = 2
+// let d = ((a+c)*(b+c))
+// console.log(d);
+
+// Begin 20
+// let x1 = 5
+// let x2 = 2
+// let y1 = 1
+// let y2 = 8
+// let d = Math.sqrt((x1-x2)**2+(y1-y2)**2)
+// console.log(d);
+
+// Begin 21
+
+
 
 
 
